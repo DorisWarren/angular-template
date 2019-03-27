c09fab557c92c34312060179e3194e3d57db8138
Student <example@example.com>
2019-03-27 16:29:19 -0700

    added new components to template - list - edit - new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,24 +12,20 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
+  selectedTask = null;
 
-  // editTask() {
-  //   alert ("Time to edit this task");
-  // }
-
-  tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus courses', 3),
-    new Task('Begin brianstorming possible JavaScript group projects', 2),
-    new Task('Add README file to last few Angular repos on Github', 2)
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
 
-  priorityColor(currentTask){
-      if (currentTask.priority === 3){
-        return "bg-danger";
-      } else if (currentTask.priority === 2) {
-        return  "bg-warning";
-      } else {
-        return "bg-info";
-      }
-    }
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
   }
+
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+
+}
